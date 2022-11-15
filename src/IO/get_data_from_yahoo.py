f9e0a16f0d5357275828a4798c42937b2864385e
Isaque <58992653+isaquecerqueira@users.noreply.github.com>
2022-11-15 14:41:37 -0500

    Update get_data_from_yahoo.py

--- src/IO/get_data_from_yahoo.py
+++ src/IO/get_data_from_yahoo.py
@@ -6,6 +6,6 @@ from yahoo_fin import stock_info as si
 def get_last_stock_price(ticker, last=False):
     if last:
         now = datetime.now()
-        start_date = now - timedelta(days=365)
+        start_date = now - timedelta(days=2000)
         return si.get_data(ticker, start_date)
     return si.get_data(ticker)
